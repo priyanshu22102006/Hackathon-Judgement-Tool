4a1d0064b44bcd2ac777dd1682594eab2dd15a5a
Pritim Mondal <mondalpritim14@gmail.com>
2026-03-01 04:59:13 +0530

    optimize for mac

--- backend/seed.js
+++ backend/seed.js
@@ -34,8 +34,8 @@ const SEED_CONFIG = {
   },
   team: {
     name: "Alpha Coders",
-    // repoFullName: "pritimmondall/demo",   // ← change this to any repo
-    repoFullName: "priyanshu22102006/Coding",
+    repoFullName: "pritimmondall/demo",   // ← change this to any repo
+    // repoFullName: "priyanshu22102006/Coding",
     members: ["alice", "bob", "charlie"],
   },
 };
